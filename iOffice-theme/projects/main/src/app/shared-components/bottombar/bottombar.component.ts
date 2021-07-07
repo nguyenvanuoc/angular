a3b620600f9e1f3b9a180a5ff4ef1b253512711c
hadongtung <hadongtung@gmail.com>
2021-07-07 21:29:07 +0700

    update color

--- iOffice-theme/projects/main/src/app/shared-components/bottombar/bottombar.component.ts
+++ iOffice-theme/projects/main/src/app/shared-components/bottombar/bottombar.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BottombarComponent implements OnInit {
   @Input() position: number = 0;
-  @Input() bgColor: string = '#fff';
+  @Input() bgColor: string = '#f0f5ff';
 
   constructor() {}
 
