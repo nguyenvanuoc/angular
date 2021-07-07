f4fe86b3fc3921d3c8e5a4ee5b722d0b8bf6db68
hadongtung <hadongtung@gmail.com>
2021-07-07 18:41:49 +0700

    update style

--- iOffice-theme/projects/main/src/app/app.component.ts
+++ iOffice-theme/projects/main/src/app/app.component.ts
@@ -9,10 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'main';
     visibleSidebar = true;
+    menuVisible: boolean = true;
     closeSidebar(): void {
       setTimeout(() => {
         window.dispatchEvent(new Event('resize'));
       }, 300);
       this.visibleSidebar = !this.visibleSidebar;
+    }
+
+    tonggleMenu() {
+      this.menuVisible = !this.menuVisible;
+      setTimeout(() => {
+        window.dispatchEvent(new Event('resize'));
+      }, 300);
+    }
+  
+    itemClick() {
+      if (window.innerWidth <= 768) {
+        this.menuVisible = true;
+      }
     }
 }
